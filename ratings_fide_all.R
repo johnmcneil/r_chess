6ac7590ca953955bca0526c56a2d3ef1b3465a65
John McNeil <johnmcneil@users.noreply.github.com>
2020-10-29 21:55:01 -0500

    Update ratings_fide_all.R

--- ratings_fide_all.R
+++ ratings_fide_all.R
@@ -1,90 +1,75 @@
 library(tidyverse)
+library(lubridate)
 
 # load csv
 # supplements for comparison
-ratings2019 <- read.csv("R_Projects/2019-05_supp.csv", header=TRUE)
-ratings2014 <- read.csv("R_Projects/2014-05_supp.csv", header=TRUE)
-str(ratings2019)
+ratings2019 <- read.csv("R_Projects/2019-05_supp.csv", header=TRUE, stringsAsFactors = FALSE)
+ratings2014 <- read.csv("R_Projects/2014-05_supp.csv", header=TRUE, stringsAsFactors = FALSE)
 
 # most recent csv of all fide ratings
 allFide <- read.csv("R_Projects/fide_ratings.csv", header=FALSE, stringsAsFactors = FALSE )
 
 # add column names to allFide
-names(allFide)
-names(ratings2019)
 names(allFide) <- c("ID", "DATE", "RATING_FORMAT", "FIDE_ID", "NAME", "FED", 
                     "SEX", "TIT", "WTIT", "OTIT", "RATING", "GMS",
                     "K", "BIRTH_YEAR", "FLAG", "BIRTH_DATE", "FOA")
-names(allFide)
 
 # set data types
-as.integer(allFide$ID)
+allFide$ID <- as.integer(allFide$ID)
 
 
-lct <- Sys.getlocale("LC_TIME"); Sys.setlocale("LC_TIME", "C");
-as.Date(allFide$DATE, "%Y-%m-%d")
-Sys.setlocale("LC_TIME", lct)
-str(allFide$DATE)
+# change date from 00 to 01 because that's what as.Date() accepts
+allFide$DATE <- str_replace(allFide$DATE, "-00", "-01")
+allFide$DATE <- as.Date(allFide$DATE)
 
-as.factor(allFide$RATING_FORMAT)
-as.integer(allFide$FIDE_ID)
-as.character(allFide$NAME)
-as.factor(allFide$FED)
-as.factor(allFide$SEX)
-as.factor(allFide$TIT)
-as.factor(allFide$WTIT)
-as.factor(allFide$OTIT)
-as.integer(allFide$RATING)
-as.integer(allFide$GMS) 
-as.integer(allFide$K)
-as.integer(allFide$BIRTH_YEAR)
-as.factor(allFide$FLAG)
-as.Date(allFide$BIRTH_DATE, "%Y-%m-%d")
-as.factor(allFide$FOA)
+
+allFide$RATING_FORMAT <- as.factor(allFide$RATING_FORMAT)
+allFide$FIDE_ID <- as.integer(allFide$FIDE_ID)
+allFide$NAME <- as.character(allFide$NAME)
+allFide$FED <- as.factor(allFide$FED)
+allFide$SEX <- as.factor(allFide$SEX)
+allFide$TIT <- as.factor(allFide$TIT)
+allFide$WTIT <- as.factor(allFide$WTIT)
+allFide$OTIT <- as.factor(allFide$OTIT)
+allFide$RATING <- as.integer(allFide$RATING)
+allFide$GMS <- as.integer(allFide$GMS) 
+allFide$K <- as.integer(allFide$K)
+allFide$BIRTH_YEAR <- as.integer(allFide$BIRTH_YEAR)
+allFide$FLAG <- as.factor(allFide$FLAG)
+allFide$BIRTH_DATE <- as.Date(allFide$BIRTH_DATE, "%Y-%m-%d")
+allFide$FOA <- as.factor(allFide$FOA)
 
 # explore data vectors
 names(allFide)
+str(allFide)
 str(allFide$ID)
 str(allFide$TIT)
 str(allFide$DATE)
 levels(allFide$TIT)
 
-summary(allFide$BIRTH_YEAR)
-hist(allFide$BIRTH_YEAR)
+# explore rating dates
+hist(allFide$DATE, breaks="years")
+eighties <- allFide %>% filter(year(allFide$DATE) > 1979
+                          & year(allFide$DATE) < 1990)
+hist(eighties$DATE, breaks="years")
 
 # what's the distribution of birth years in the ratings records?
 birthYear <- allFide %>% filter(allFide$BIRTH_YEAR != 0)
 hist(birthYear$BIRTH_YEAR)
 
-# look at standard ratings only. summarize average rating by date
-allFideStandard <- allFide %>% filter(allFide$RATING_FORMAT == "standard")
-aveRatingByDate <- allFideStandard %>% group_by(DATE) %>% summarise(AVE_RATING = mean(RATING))
 
 # look at standard ratings above 2200 only. summarize average rating by date
-allFideStandardMaster <- allFideStandard %>% filter(allFideStandard$RATING_FORMAT == "standard"
-                                      & allFideStandard$RATING > 2199
-                                      & allFideStandard$FLAG != "i")
-aveRatingByDateMaster <- allFideStandardMaster %>% group_by(DATE) %>% summarise(AVE_RATING = mean(RATING))
-ggplot(aveRatingByDateMaster, aes(x=DATE, y=AVE_RATING))+
-         geom_point()
+allFideStandardMaster <- allFide %>% filter(allFide$RATING_FORMAT == "standard"
+                                      & allFide$RATING > 2199
+                                      & allFide$FLAG != "i")
+aveFideStandardMaster <- allFideStandardMaster %>% group_by(DATE) %>% summarise(AVE_RATING = mean(RATING))
+ggplot(aveFideStandardMaster, aes(x=DATE, y=AVE_RATING)) +
+  geom_point() +
+  scale_x_date(breaks = "10 years")
+    
 
 
-# for standard ratings above 2400, summarize average rating by date
-allFideStandard2400plus <- allFideStandardMaster %>% filter(allFideStandardMaster$RATING > 2399
-                                                            & allFideStandardMaster$FLAG != "i")
-aveRatingByDate2400plus <- allFideStandard2400plus %>% group_by(DATE) %>% summarise(AVE_RATING = mean(RATING))
-ggplot(aveRatingByDate2400plus, aes(x=DATE, y=AVE_RATING))+
-  geom_point()
-
-# for standard ratings above 2600, summarize average rating by date
-allFideStandard2600plus <- allFideStandardMaster %>% filter(allFideStandardMaster$RATING > 2599
-                                                            & allFideStandardMaster$FLAG !="i")
-aveRatingByDate2600plus <- allFideStandard2600plus %>% group_by(DATE) %>% summarise(AVE_RATING = mean(RATING))
-ggplot(aveRatingByDate2600plus, aes(x=DATE, y=AVE_RATING)) +
-  geom_point()                                                  
-
-
-# does the change over time in average rating differ by gender?
+# how does the average change in rating compare by gender?
 allFideStandardMasterMale <- allFideStandardMaster %>% filter(allFideStandardMaster$SEX == "M")
 aveRatingsByDateMasterMale <- allFideStandardMasterMale %>% group_by(DATE) %>% summarise(AVE_RATING = mean(RATING))
 ggplot(aveRatingsByDateMasterMale, aes(x=DATE, y=AVE_RATING)) +
@@ -95,21 +80,21 @@ aveRatingsByDateMasterFemale <- allFideStandardMasterFemale %>% group_by(DATE) %
 ggplot(aveRatingsByDateMasterFemale, aes(x=DATE, y=AVE_RATING)) +
   geom_point()
 
+# compare average rating of all known gender by date
+genderKnown <- allFide %>% filter(allFide$SEX != "" 
+                                  & allFide$FLAG != "i"
+                                  & RATING > 1999)
+genderKnownSummary <- genderKnown %>% group_by(DATE, SEX) %>% summarise(AVE_RATING = mean(RATING))
+ggplot(genderKnownSummary, aes(x=DATE, y=AVE_RATING, col=SEX)) +
+  geom_point() +
+  ggtitle("Mean rating of players at or above 2000 FIDE, by gender")
 
-male <- allFideStandard2600plus %>% filter(allFideStandard2600plus$SEX == "M")
-ave_male <- male %>% group_by(DATE) %>% summarise(AVE_RATING = mean(RATING))
-ggplot(ave_male, aes(x=DATE, y=AVE_RATING)) +
-  geom_point()
+# compare number of all known gender by date
+genderCount <- genderKnown %>% group_by(DATE, SEX) %>% summarise(COUNT = n())
+ggplot(genderCount, aes(x=DATE, y=COUNT, col=SEX)) +
+  geom_point() +
+  ggtitle("Count of players at or above 2000 FIDE by gender")
 
-female <- allFideStandard2600plus %>% filter(allFideStandard2600plus$SEX == "F")
-ave_female <- female %>% group_by(DATE) %>% summarise(AVE_RATING = mean(RATING))
-ggplot(ave_female, aes(x=DATE, y=AVE_RATING)) +
-  geom_point()
-
-# how has the number of top female players changed?
-gender <- allFideStandardMaster %>% group_by(DATE, SEX) %>% summarize(n())
-
-femaleMastersByDate <- allFideStandardMastersFemale %>% group_by(DATE) %>% summarise(N_FEMALE = )
 
 # use colors!
 allFideStandard2600plusGendered <- allFideStandard2600plus %>% 
@@ -119,7 +104,7 @@ ggplot(allFideStandard2600plusGendered, aes(x=DATE, y=RATING, col=SEX)) +
   geom_point() + 
   scale_x_date(breaks = "10 years")
 
-?allFideStandard2600plusFED <- allFideStandard2600plus %>%
-  filter(allFideStandard2600plus$FED != "")
-ggplot(allFideStandard2600plusFED, aes(x=DATE, y=RATING, col=FED)) +
+allFideStandardMaster <- allFideStandardMaster %>%
+  filter(allFideStandardMaster$FED != "")
+ggplot(allFideStandardMaster, aes(x=DATE, y=RATING, col=FED)) +
   geom_point()
