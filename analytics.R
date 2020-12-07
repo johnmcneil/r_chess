c944f81b20467af38965f1f95f6c510a30a21c43
John McNeil <johnmcneil@users.noreply.github.com>
2020-12-06 21:45:12 -0600

    Update analytics.R
    starting to revise the analytics for all logs

--- analytics.R
+++ analytics.R
@@ -114,14 +114,13 @@ ggplot(all_logs, aes(x=Time, y=Number_of_names_searched)) +
   geom_point()
 
 
-referrer <- table(oct2020$HTTP_REFERRER)
+referrer <- table(all_logs$HTTP_REFERRER)
 view(referrer)
 
-format <- table(oct2020$format)
+format <- table(all_logs$format)
 barplot(format)
 
 # explore rating formats
-summary(all_logs$format)
 allFideStandard <- all_logs %>% filter(format == "fide-standard")
 allFideRapid <- all_logs %>% filter(format == "fide-rapid")
 allFideBlitz <- all_logs %>% filter(format == "fide-blitz")
